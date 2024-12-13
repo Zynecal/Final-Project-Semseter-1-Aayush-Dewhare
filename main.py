54c66376196730dec6a1edc9b92cd84e8f08cd47
Aayush Dewhare <108435412+Zynecal@users.noreply.github.com>
2024-12-13 10:34:27 -0800

    12/13

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import requests
-from tkinter import *
-
-def newWindow():
-    # create root window
-    root = Tk()
-
-    # root window title and dimension
-    root.title('TaskFlow - Interactive Homework Tracker')
-
-    # set geometry 
-    root.geometry('1920x1080')
-
-    # execute tkinter
-    root.mainloop()
+import tkinter as tk
 
 # establish url and api token
 canvas_url = "https://dublinusd.instructure.com/"
 api_token = "8844~UZE83EW8BN2W6CQzNKnL2Nw3kFtvPFWMnrDXcLZPNA8hGDTyGmKvc6mwvX9RPXCt"
 course_id = "19099"
+assignment_names = []
 
 # create an API endpoint
 url  = f'{canvas_url}/api/v1/courses/{course_id}/assignments'
@@ -40,7 +28,30 @@ if response.status_code == 200:
     assignments = response.json()
     print('Assignments:')
 
+    
+
+def newWindow():
+    # create window
+    window = tk.Tk()
+
+    # window title and dimension
+    window.title('TaskFlow - Interactive Homework Tracker')
+
+    # set geometry 
+    window.geometry('900x750')
+    
     for assignment in assignments:
-        print(f'Assignment Name: {assignment['name']}')
-        print(f"Due Date: {assignment['due_at']}")
+        #print(f'Assignment Name: {assignment['name']}')
+        #print(f"Due Date: {assignment['due_at']}")
+
+        assignmentName = (f'Assignment Name: {assignment['name']}')
+        assignmentDueDate = (f"Due Date: {assignment['due_at']}")
+        assignment_names.append(assignmentName)
+
+        tk.Label(window, text=(assignmentName), font=("Arial", 16)).pack()
+
+    # execute tkinter
+    window.mainloop()
+
+newWindow()
 
